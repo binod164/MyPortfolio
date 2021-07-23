8d5d828410703476195f405ea1e9a1ee6796a1dc
Binod Gurung <binod7_grg@hotmail.com>
2021-07-23 16:38:49 -0500

    made changes in package component

--- my-app/src/component/Packages.js
+++ my-app/src/component/Packages.js
@@ -3,16 +3,41 @@ import React from 'react'
 function packages() {
     return (
        <div className="packages">
+           
         <div className="column">
-            <img src="https://peakvisor.com/photo/Nepal-Khumbu-valley-Solukhumbu.jpg" width = "300" height = "300"/><br></br>
+             <img src="https://peakvisor.com/photo/Nepal-Khumbu-valley-Solukhumbu.jpg" width = "400" height = "400"/><br></br>
+             <p ><b>7 days trip to the beautiful Mountains</b></p>
+            <p> Includes climbing kits, food and lodging</p>
+            <button class = "button1" type="submit" className="btn">Select</button>
+        </div>
+        <div className="column">
+            <img src="http://volunteerworknepal.com/wp-content/uploads/2018/08/Patan-Durbar-Square-Cultural-Heritage-of-Nepal.jpg" width = "400" height = "400"/><br></br>
+            <p><b>7 days trip to the World Heritages</b></p>
+            <p> Includes food,transportation and lodging</p>
             <button type="submit" className="btn">Select</button>
         </div>
         <div className="column">
-            <img src="http://volunteerworknepal.com/wp-content/uploads/2018/08/Patan-Durbar-Square-Cultural-Heritage-of-Nepal.jpg" width = "300" height = "300"/><br></br>
+            <img src="https://www.itournepal.com/images/tihar.jpg" width = "400" height = "400"/><br></br>
+            <p><b>Celebrate the national festivals of Nepal</b></p>
+            <p> Includes food and lodging</p>
             <button type="submit" className="btn">Select</button>
         </div>
         <div className="column">
-            <img src="https://www.itournepal.com/images/tihar.jpg" width = "300" height = "300"/><br></br>
+            <img src="https://media.tacdn.com/media/attractions-splice-spp-674x446/06/74/b8/1b.jpg" width = "400" height = "400"/><br></br>
+            <p><b>3 days trip to Chitwan</b></p>
+            <p> Includes food, jungle safari and lodging</p>
+            <button type="submit" className="btn">Select</button>
+        </div>
+        <div className="column">
+            <img src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/colorful-nepal-boats-parking-in-phewa-lake-pokhara-joseph-mathew.jpg" width = "400" height = "400"/><br></br>
+            <p><b>3 days trip to Pokhara </b></p>
+            <p> Includes food, boating and lodging</p>
+            <button type="submit" className="btn">Select</button>
+        </div>
+        <div className="column">
+            <img src="https://www.tripsavvy.com/thmb/zcjtBbw7eBHrjvzj0qo1KET0T3U=/2121x1414/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-555584883-59098b2a3df78c92835c8def.jpg"width = "400" height = "400"/><br></br>
+            <p><b>3 days trip to Kathmandu</b> </p>
+            <p> Includes food and lodging</p>
             <button type="submit" className="btn">Select</button>
         </div>
     </div>
--- my-app/src/component/ThingsToDo.js
+++ my-app/src/component/ThingsToDo.js
@@ -9,15 +9,80 @@ function ThingsToDo() {
                     
                               <Carousel.Item>
                                 <img className = "todo" src="https://i.pinimg.com/originals/51/5a/75/515a757fbe92d8273442fdaab02dcf04.png"  width="1800" height="900"/>
-                               
+                                <h1>VISIT WORLD HERITAGES OF NEPAL</h1><p>Nepal is a multilingual and multicultural country that preserves hundreds of cultural heritages of historical importance. This country known throughout the world as the home of Mount Everest and Lord Buddha is an intricate garland of the various ethnicity and castes of people who have inhabited the land for millennia.
+
+                                    The country is ruled by diverse dynasties like the Kirant, Lichchhavis, Mallas, and Shah – all with their own distinct cultures and architectures. And these rulers have left various natural as well as religious, cultural, and architectural heritage.
+
+                                    These historically important patrimonies provide a glimpse into the rich art and culture of the country and have, therefore, been placed under the World Heritage list by UNESCO. The 10 UNESCO World Heritages sites in Nepal are:</p>
+                               <ul>
+                                 <li>Pashupatnath Temple</li>
+                                 <li>Swayambhunath Stupa</li>
+                                 <li>Boudhanath Stupa</li>
+                                 <li>Kathmandu Durbar Square</li>
+                                 <li>Patan Durbar Square</li>
+                                 <li>Bhaktapur Durbar Square</li>
+                                 <li>Changu Narayan Temple</li>
+                                 <li>Lumbini</li>
+                                 <li>Chitwan National park</li>
+                                 <li>Sagarmatha National Park</li>
+
+                               </ul>
+                               <p>For more informations click on the link provided below</p>
+                               <a href="https://www.mountainmarttreks.com/blog/10-unesco-world-heritage-sites/">Top 10 UNESCO World Heritage Sites In Nepal
+                               </a>
                               </Carousel.Item>
                             
                     
                               <Carousel.Item>
                                 <img className = "todo" src="https://images.squarespace-cdn.com/content/v1/53ecd1bde4b0a6f9524254f8/1468123073306-QVCM88JW71C83O3XO748/ke17ZwdGBToddI8pDm48kJfSPtfOJWis-4HwS2UffyIUqsxRUqqbr1mOJYKfIPR7qPMk0p3IHmLoUpLadMHyzIZ_PIT2Qv94SwsoGTQcqCErN1-8Q_X9ldEXlc8wrw7gInPi9iDjx9w8K4ZfjXt2dnFkZJQFgiGj4-1xI4Lcxm0isKgA55dC5MHRlyyS51k_ZDqXZYzu2fuaodM4POSZ4w/adve.jpg?format=1500w" width="1800" height="900"/>
+                              <h1> ADVENTURES IN NEPAL</h1><p>Well known for being one of the most awesome adventure activity locations in Asia, Nepal is a land of high mountains, deep valleys, rushing rivers, and dense jungles. And for an intrepid adventurer, there are few places better in the world for that all-important adrenaline rush. The unique landscape of Nepal, from the high Himalayan mountains to the lowland jungles, allows for a wide range of adventure activities, from trekking to Mount Everest and the Annapurna Range to skydiving and white-water rafting down some of the world’s best rivers. If you love adrenaline sports, the following adventurous things should definitely not to be missed.</p>
+                              <ul>
+                                 <li>Waterfall Ice Climbing</li>
+                                 <li>Trekking Mount Everest</li>
+                                 <li>Bungee Jumping & Canyon Swing</li>
+                                 <li>Mountain Flight Around Nepal</li>
+                                 <li>Parahawking</li>
+                                 <li>Jungle Safari</li>
+                                 <li>Sky Dive Over Everest</li>
+                               </ul>
+                               <p>For more informations click on the link provided</p>
+                               <a href="https://www.adventureinyou.com/nepal/10-insanely-awesome-adventures-in-nepal/"> 10 Insanely Awesome Adventures in Nepal
+                              </a>
                               </Carousel.Item>
                               <Carousel.Item>
                                 <img className = "todo" src="https://booking-manager-api.s3.eu-west-1.amazonaws.com/27/blob" width="1800" height="900"/>
+                                <h1>CELEBRATE FESTIVALS OF NEPAL</h1><p>Nepal holidays and festivals concentrate on religious and cultural events on the Hindu and Buddhist calendars and involve traditional rituals, music and dance. Due to the number of diverse ethnic and tribal groups making up the population, the variety of celebrations is fascinating for visitors. Dashain in early September is the most important, surrounded by many other events, and the Tihar Hindu festival of lights is one of the loveliest.
+                                <br></br>
+                                <strong>Shree Panchami</strong><br></br>
+                                 This celebration of Saraswati, the goddess of learning and sister of the elephant god Ganesh, takes place in Nepal in January. Temple images are garlanded, presented with gifts and seven grains of rice are eaten by worshippers in order to gain wisdom. It’s the most auspicious time for weddings with countless processions on the streets, as the marriage is blessed by the goddess herself.
+                                <br></br>
+                                <strong>Ghode Jatra</strong><br></br>
+                                  Held in mid-March, Ghode Jatra is the horse race event of Tundikhel, with a grand horse parade honoring a victory over a dangerous demon crushed by the hooves of local fillies. Legend has it, the event keep the demon’s spirit from returning. Another race involves an intoxicated horse and drunk rider, with Nepalese townspeople cheering on the unsteady twosome. The rider desperately tries to hang on for as long a he can, causing universal merriment.
+                                <br></br>
+                                 <strong>Holi</strong><br></br>
+                                  The joyous spring festival of Holi takes place in March, and is known as the feast of colors. Based on a victory over the female demon Holika, the celebrations last a week and are a time of eating, drinking and rejoicing in the streets to welcome the warmer weather. Citizens smear themselves with brilliant, powdered colors and throw the colors over passerby’s. Bonfires and more revels end the celebration.
+                                <br></br>
+                                <strong>Mata Turtha Puja</strong><br></br>
+                                  Closely-knit families are a long-established tradition in Nepal, with the early May festival of Mata Turtha Puja the Nepalese ‘Mothers Day’. Honored for their love and support with gifts, adult sons and daughters return to their family home to show appreciation.
+                                <br></br>
+                                <strong>Buddha Jayanti</strong><br></br>
+                                   Buddhist temples are packed with worshippers on Jayanti, the celebration of the Buddha’s birthday. Held in June on a full moon day, the Nepalese festival honors the Buddha’s birth, death, and attainment of enlightenment. Huge images of the god are displayed and parades of devotees praising his life and teachings take place.
+                                <br></br>
+                                <strong>Gaijatra</strong><br></br>
+                                   Every year in late August/early September, Gaijatra honors the god of death, Yamaraj. It’s one of the most popular festivals for its procession of cows led by every person who has lost a relative during the previous year. For Hindus, cows are holy and it’s believed the parade will help the souls of the departed journey to heaven.
+                                <br></br>
+                                <strong>Indra Jatra</strong><br></br>
+                                    Taking place in late August/early September for eight days, the combined Hindu and Buddhist festival of Indra Jatra happens in Kathmandu. A celebration of traditional Nepalese classical dance, each community performs its own unique sequences in honor of the king of heaven, Lord Indra. On the third day, the living goddess Kumari is paraded through the city on a chariot, the processional ending at Durbar Square.
+                                <br></br>
+                                <strong>Krishna Janmastami</strong><br></br>
+                                    Across Nepal, Sri Krishna, the eighth incarnation of Lord Vishnu, is celebrated on his birthday in September especially in Patan, home of the ancient Krishna Temple. Citizens huddle together all night long, keeping vigilant and chanting the various names of the god or singing traditional hymns. Flowers, food and money are offered to Krishna’s image in the gloomy main temple.
+                                <br></br>
+                                <strong>Dashain</strong><br></br>
+                                    Held over two weeks at the end of September, Dashain is Nepal’s most important festival, celebrated across the land and ending on the day of the full moon. Dashain honors the victory of the gods over a host of demons, based on tales in the Ramayana, after invoking the powers of the goddess Durga. Tantric rites are conducted as mother goddess temple offerings are made and specific rituals take place in all homes, towns and cities every day of the event.
+                                <br></br>
+                                <strong>Tihar</strong><br></br>
+                                    Tihar is the Nepalese version of the Hindu Festival of Lights, dedicated to the goddess of wealth, Lakshmi, and is the most dazzling of Nepal’s festivals. It takes place for five days in October, during which every home is lit up with sparkling oil lamps. The second day is known as ‘dogs’ day’ and every four legged friend is adorned with a floral garland, given a delicious meal and worshipped for its protection of the home. Cows are given similar treatment on the third day, as representatives of wealth.</p>
+                               
                               </Carousel.Item>
                               
                 </Carousel>
