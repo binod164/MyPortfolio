27e044794f46b7f34805069d0d00c12eef8d841d
Binod Gurung <46802930+binod164@users.noreply.github.com>
2021-09-16 16:33:32 -0500

    Update NewUser.js

--- my-app/src/component/NewUser.js
+++ my-app/src/component/NewUser.js
@@ -3,26 +3,23 @@ import React from 'react'
 function NewUser() {
     return (
        <div class ="container">
-         <img class = "about" src = "http://www.beautiful-women-pedia.com/images/nepal.jpg"/>
-         <div class="centered">
-        <h1><b>SignUp</b></h1>   
-         <p>Enter your full name</p>
-       <input id="fullname" placeholder="Enter fullname" type="text"></input>
-       <p>Enter your email address</p>
-       <input id="emailAddress" placeholder="Enter email" type="email"></input>
-       <p>Enter your password</p>
-       <input id="password" placeholder="Enter password" type="text"></input>
-       <p>Re-enter your password</p>
-       <input id="password" placeholder="Reenter password" type="text"></input>
-         <br></br>
-         <br></br>
-       <button type="submit" className="btn">Login</button> <br />
-       <br></br>
-
-           
-
-         </div>
-       </div>
+             <img class = "about" src = "http://www.beautiful-women-pedia.com/images/nepal.jpg"/>
+                <div class="centered">
+                     <h1><b>SignUp</b></h1>   
+                     <p>Enter your full name</p>
+                     <input id="fullname" placeholder="Enter fullname" type="text"></input>
+                     <p>Enter your email address</p>
+                     <input id="emailAddress" placeholder="Enter email" type="email"></input>
+                     <p>Enter your password</p>
+                     <input id="password" placeholder="Enter password" type="text"></input>
+                     <p>Re-enter your password</p>
+                     <input id="password" placeholder="Reenter password" type="text"></input>
+                     <br></br>
+                     <br></br>
+                     <button type="submit" className="btn">Login</button> <br />
+                     <br></br>
+                </div>
+      </div>
     )
 }
 
