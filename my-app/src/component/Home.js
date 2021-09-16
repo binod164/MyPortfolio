d253dcf8464e160c6c15cb0653df69614a4ce0ce
Binod Gurung <46802930+binod164@users.noreply.github.com>
2021-09-16 16:34:40 -0500

    Update Home.js

--- my-app/src/component/Home.js
+++ my-app/src/component/Home.js
@@ -3,22 +3,20 @@ import { NavLink } from 'react-router-dom';
 const home = () => {
     return (
        <div class ="container">
-         <img class = "about" src = "http://www.beautiful-women-pedia.com/images/nepal.jpg"/>
-         <div class="centered">
-        <h1><b>Namaste Nepal</b></h1>    <br></br>
-       <p>Enter your email address</p>
-       <input id="emailAddress" placeholder="Enter email" type="email"></input>
-       <p>Enter your password</p>
-       <input id="password" placeholder="Enter password" type="text"></input>
-          <br></br>
-         <br></br>
-       <button type="submit" className="btn">Login</button> <br />
-       <br></br>
-       
-           <NavLink to="/newuser">New User? Create an account</NavLink>
-
-         </div>
-       </div>
+            <img class = "about" src = "http://www.beautiful-women-pedia.com/images/nepal.jpg"/>
+                 <div class="centered">
+                     <h1><b>Namaste Nepal</b></h1>    <br></br>
+                     <p>Enter your email address</p>
+                     <input id="emailAddress" placeholder="Enter email" type="email"></input>
+                     <p>Enter your password</p>
+                     <input id="password" placeholder="Enter password" type="text"></input>
+                     <br></br>
+                     <br></br>
+                     <button type="submit" className="btn">Login</button> <br />
+                     <br></br>
+                     <NavLink to="/newuser">New User? Create an account</NavLink>
+                  </div>
+      </div>
     );
 }
  
